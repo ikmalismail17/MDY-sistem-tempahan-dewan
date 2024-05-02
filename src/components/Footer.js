c2f7a3e3d488c6d246396e3b31eb63a9cd8bb855
ikmalismail17 <ikmal5a2013@gmail.com>
2024-05-02 10:08:26 +0800

    update new footer, configure calendar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,29 @@
+import Logo from '../assets/images/logoMDY.png';
+
 const Footer = () => {
     let currentYear = new Date().getFullYear();
 
     return (
         <>
-        <footer className="footer footer-center p-4 bg-base-100 text-base-content">
-            <aside>
-                <p>Copyright © {currentYear} Majlis Daerah Yan</p>
-            </aside>
+        <footer className="footer footer-center p-10 bg-gray-600 text-primary-content">
+        <aside>
+            <div className="avatar">
+                <div className="w-20 mask mask-squircle" >
+                    <img src={Logo} alt='Majlis Daerah Yan' />
+                </div>
+            </div>
+            <p className="font-bold">
+            Majlis Daerah Yan <br/>06900 Yan, Kedah
+            </p> 
+            <p>Copyright © {currentYear} - MDY</p>
+        </aside> 
+        <nav>
+            <div className="grid grid-flow-col gap-4">
+            <a href='https://twitter.com/MajlisY'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
+            <a href='https://pbt.kedah.gov.my/index.php/majlis-daerah-yan/'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"><path d="M21.721 12.752a9.711 9.711 0 0 0-.945-5.003 12.754 12.754 0 0 1-4.339 2.708 18.991 18.991 0 0 1-.214 4.772 17.165 17.165 0 0 0 5.498-2.477ZM14.634 15.55a17.324 17.324 0 0 0 .332-4.647c-.952.227-1.945.347-2.966.347-1.021 0-2.014-.12-2.966-.347a17.515 17.515 0 0 0 .332 4.647 17.385 17.385 0 0 0 5.268 0ZM9.772 17.119a18.963 18.963 0 0 0 4.456 0A17.182 17.182 0 0 1 12 21.724a17.18 17.18 0 0 1-2.228-4.605ZM7.777 15.23a18.87 18.87 0 0 1-.214-4.774 12.753 12.753 0 0 1-4.34-2.708 9.711 9.711 0 0 0-.944 5.004 17.165 17.165 0 0 0 5.498 2.477ZM21.356 14.752a9.765 9.765 0 0 1-7.478 6.817 18.64 18.64 0 0 0 1.988-4.718 18.627 18.627 0 0 0 5.49-2.098ZM2.644 14.752c1.682.971 3.53 1.688 5.49 2.099a18.64 18.64 0 0 0 1.988 4.718 9.765 9.765 0 0 1-7.478-6.816ZM13.878 2.43a9.755 9.755 0 0 1 6.116 3.986 11.267 11.267 0 0 1-3.746 2.504 18.63 18.63 0 0 0-2.37-6.49ZM12 2.276a17.152 17.152 0 0 1 2.805 7.121c-.897.23-1.837.353-2.805.353-.968 0-1.908-.122-2.805-.353A17.151 17.151 0 0 1 12 2.276ZM10.122 2.43a18.629 18.629 0 0 0-2.37 6.49 11.266 11.266 0 0 1-3.746-2.504 9.754 9.754 0 0 1 6.116-3.985Z" /></svg></a>
+            <a href='https://www.facebook.com/mdyan06900'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+            </div>
+        </nav>
         </footer>
         </>
     );
--- src/components/HallSec.js
+++ src/components/HallSec.js
@@ -2,8 +2,9 @@ import React, {useState, useRef} from 'react';
 import Paper from '../components/Paper';
 import { CSSTransition } from 'react-transition-group';
 import "../assets/styles/MainSec.css";
-import { Calendar } from 'rsuite';
+import { Calendar, Badge } from 'rsuite';
 import "rsuite/dist/rsuite.min.css";
+import dayjs from 'dayjs';
 
 const listDewan = [
     {
@@ -11,11 +12,47 @@ const listDewan = [
         // img: "https://yan.pimaxis.my/images/Aktiviti/submenu/KEMUDAHAN%20AWAM/GAMBAR%2014-%20DEWAN%20DATO'%20MADI%20YAN.JPG",
         img: "https://fastly.4sqi.net/img/general/width960/LYBRK3IF2DLIAVDAUAI4FDVZOIOVVVEDDPFKIVKJDCVMZQGC.jpg",
         desc: "Dewan Dato' Madi merupakan dewan yang terletak di berdekatan dengan Majlis Daerah Yan.",
+        bookdetails: [
+            {
+                dateBook: "4-18-2024",
+                status: "disahkan",
+            },
+            {
+                dateBook: "5-12-2024",
+                status: "ditempah",
+            },
+            {
+                dateBook: "5-15-2024",
+                status: "disahkan",
+            },
+            {
+                dateBook: "5-29-2024",
+                status: "ditempah",
+            },
+        ]
     },
     {
         title: "Dewan Lestari",
         img: "https://lh3.googleusercontent.com/p/AF1QipOAVYw-MlVynOz-qb0gLoCTaOGbYf6AjnOUVmU3=s680-w680-h510",
         desc: "Dewan Lestari merupakan dewan yang terletak di kawasan Guar Chempedak.",
+        bookdetails: [
+            {
+                dateBook: "4-28-2024",
+                status: "ditempah",
+            },
+            {
+                dateBook: "5-16-2024",
+                status: "disahkan",
+            },
+            {
+                dateBook: "5-30-2024",
+                status: "ditempah",
+            },
+            {
+                dateBook: "6-3-2024",
+                status: "ditempah",
+            },
+        ]
     },
 ];
 
@@ -27,13 +64,14 @@ const HallSec = () => {
     const [hallDetailData, setHallDetail] = useState({
         title: "",
         img: "",
-        // details:{}
+        bookdetails: []
     });
 
     const handleHallDetailData = (title, img) => {
         setHallDetail({
             title: title,
-            img: img
+            img: img,
+            bookdetails: listDewan.find(dewan => dewan.title === title).bookdetails
         });
     }
 
@@ -98,6 +136,29 @@ const HallSec = () => {
 
 const HallSecDetail = (props) => {
 
+    const renderCell = (date) => {
+        const formattedDate = dayjs(date).format('DD-MM-YYYY'); // Format the current date as "DD-MM-YYYY" for comparison
+    
+        const matchingDates = props.datadetail.bookdetails.filter(item => {
+            // Convert the item's date to "DD-MM-YYYY" format for comparison
+            const formattedItemDate = dayjs(item.dateBook).format('DD-MM-YYYY');
+            return formattedItemDate === formattedDate;
+        });
+    
+        if (matchingDates.length > 0) {
+            // Display badges for matching dates
+            return (
+                <div>
+                    {matchingDates.map((item, index) => (
+                        <Badge key={index} color={item.status === 'disahkan' ? 'green' : 'orange'} content={item.status === 'disahkan' ? 'Disahkan' : 'Ditempah'} />
+                    ))}
+                </div>
+            );
+        }
+        return null;
+    };
+    
+
     const handleDetailHall = () => {
         props.hall();
     }
@@ -113,7 +174,7 @@ const HallSecDetail = (props) => {
                 </button>
                 <div role="alert" className="alert alert-info w-auto">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                    <span>Log masuk dahulu untuk menempah dewan !!</span>
+                    <span>Log masuk dahulu untuk melakukan tempahan dewan !!</span>
                 </div>
             </div>
             <div className="hero-content flex-col lg:flex-row">
@@ -123,7 +184,7 @@ const HallSecDetail = (props) => {
             </div>
             <div className="flex px-2 py-2 bg-blue-600">
                 <div className='border rounded-lg bg-slate-200 w-auto'>
-                    <Calendar bordered />
+                    <Calendar bordered renderCell={renderCell}/>
                 </div>
             </div>
             </div>
