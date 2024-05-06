eb4c0014b5c95b69a7919af0728d7c1ae8bb65a5
ikmalismail17 <ikmal5a2013@gmail.com>
2024-05-06 08:23:18 +0800

    adding new route and change few route name

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -6,17 +6,30 @@ import React from 'react';
 import { Route, Routes, useLocation} from 'react-router-dom';
 import SignIn from './SignIn';
 import Dashboard from './Dashboard';
+import BookSec from './BookSec';
+import BookList from './BookList';
+import DashboardMain from './DashboardMain';
+import Profile from './Profile';
 
 const AppContent = () => {
     const route = useLocation();
 
     let mainContent;
     switch(route.pathname){
-        case '/signin':
+        case '/logmasuk':
             mainContent = <SignIn/>;
             break;
-        case '/dashboard':
-            mainContent = <Dashboard />;
+        case '/halaman':
+            mainContent = <Dashboard content={<DashboardMain/>} titleDashboard="Halaman"/>;
+            break;
+        case '/halaman/profil':
+            mainContent = <Dashboard content={<Profile/>} titleDashboard="Profil"/>;
+            break;
+        case '/halaman/kalendartempahan':
+            mainContent = <Dashboard content={<BookSec/> } titleDashboard="Kalendar Tempahan"/>;
+            break;
+        case '/halaman/senaraitempahan':
+            mainContent = <Dashboard content={<BookList/> } titleDashboard="Senarai Tempahan"/>;
             break;
         default:
             mainContent = (
@@ -32,7 +45,7 @@ const AppContent = () => {
     return (
         <Routes>
           <Route path="/*" element={mainContent} />
-          <Route path="/dashboard/*" element={mainContent}/>
+          <Route path="/halaman/*" element={mainContent}/>
         </Routes>
     );
 }
